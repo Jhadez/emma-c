c2df2d633c6111f5fbb737c7ece1114ee0b0b6c6
Jhadez <ralegarcia@hotmail.es>
2019-11-19 14:13:41 -0600

    Update xlsx.js

--- src/public/js/xlsx.js
+++ src/public/js/xlsx.js
@@ -12,20 +12,3 @@ xlsx.writeFile(wb, `${__dirname}/${"ahs - template file.xlsx"}`);
 let newWS = xlsx.utils.sheet_to_csv(wb.Sheets["Sheet1"]);
 
 console.log("CSV ------ > "+JSON.stringify(newWS));
-
-
-// var ws = wb.Sheets["data"];
-
-// var data = xlsx.utils.sheet_add_json(data);
-
-// var newData = data.map(function (record) {
-// 	return record;
-// });
-
-// console.log(newData);
-
-// var newWB = xlsx.utils.book_new();
-// var newWS = xlsx.utils.sheet_to_csv(newData);
-// xlsx.utils.book_append_sheet(newWB, newWS, "New Data");
-
-// xlsx.writeFile(newWB, "new data file.csv");
